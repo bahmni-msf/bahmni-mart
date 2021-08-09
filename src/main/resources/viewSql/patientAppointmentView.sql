2b7fdf644c874a7a682a81d8253f17dc2b98c98f
tarunkumar-tw <tarunkumar.s@thoughtworks.com>
2021-08-09 13:05:52 +0530

    Pritam, Tarun | MOBN-1735 | Remove the dependency of patient attribute details to update appointment details in patient appointment view

--- src/main/resources/viewSql/patientAppointmentView.sql
+++ src/main/resources/viewSql/patientAppointmentView.sql
@@ -20,4 +20,4 @@ SELECT
   pap.appointment_kind
 FROM person_details_default pd
   LEFT JOIN person_attributes pa ON pa.person_id = pd.person_id
-  LEFT JOIN patient_appointment_default pap ON pap.patient_id = pa.person_id
+  LEFT JOIN patient_appointment_default pap ON pap.patient_id = pd.person_id
